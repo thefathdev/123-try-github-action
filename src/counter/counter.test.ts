4424956480c94adf55b28b756ca7d87ab8bf88e5
Fathul Irfaan Abdillah <abdillahfathu@gmail.com>
2023-03-03 07:50:28 +0700

    this should break it

--- src/counter/counter.test.ts
+++ src/counter/counter.test.ts
@@ -4,3 +4,9 @@ import { counter } from "./counter";
 test("counter increment the given number", () => {
   expect(counter(0, "increment")).toBe(1);
 });
+test("counter decrement the given number", () => {
+  expect(counter(0, "decrement")).toBe(-1);
+});
+test("counter double the given number", () => {
+  expect(counter(1, "double")).toBe(2);
+});
--- src/counter/counter.ts
+++ src/counter/counter.ts
@@ -1,10 +1,16 @@
-export function counter(num: number, method?: "increment" | "decrement") {
+export function counter(
+  num: number,
+  method?: "increment" | "decrement" | "double"
+) {
   switch (method) {
     case "increment":
       return num + 1;
 
     case "decrement":
       return num - 1;
+
+    case "double":
+      return num * 3;
 
     default:
       return num;
